47728a79231843cbdc04eef0b0d9e628df5c5bc9
SMishke <s_mishke@mail.ru>
2024-07-20 01:54:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#"1st program"
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98, 1000 != 1000.1)
+
+#3rd program
+a = 1234%1211
+b = 5678%5611
+print(a+b)
+
+#4th program
+a = 13.42
+b = 42.13
+print(int(a) == int(b % 1*100)) and (int(a%1*100+1) == int (b))
+
+
+
+
