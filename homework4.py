a921ce0bf2699586560563f269701a4523ef0f6b
Sergei <s_mishke@mail.ru>
2024-07-22 23:21:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+mi_string = input('Напишите Ваше имя ')
+print(len(mi_string))
+print(mi_string.upper())
+print(mi_string.lower())
+print(mi_string.replace(' ', ''))
+print(mi_string[0])
+print(mi_string[-1])
