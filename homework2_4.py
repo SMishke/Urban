e810483f5327cdcf72854a7ecbb4c127e4d397ad
Sergei <s_mishke@mail.ru>
2024-07-29 11:33:40 +0300

    Add files via upload

---
+++ homework2_4.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = True
+for number in numbers:
+    if number < 2:
+        continue
+    is_prime = True
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
+
